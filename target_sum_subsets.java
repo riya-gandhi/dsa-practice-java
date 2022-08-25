13cfd3867b1f855dcb606894aad8d9d7a2ad521a
Riya Gandhi <riya090401@gmail.com>
2022-08-25 19:25:17 +0530

    why traversal on entire array required for target sum subsets

--- target_sum_subsets.java
+++ target_sum_subsets.java
@@ -1,46 +1,13 @@
-// import java.util.*;
+// why traveral on entire array is necessary
+// for tar==2 in [2,0,0]
+// poss solutions are[2,0],[2],[0,2];
+// my sol would only print [2]
 
-// public class target_sum_subsets {
-
-//     public static void main(String[] args) {
-
-//         Scanner scn = new Scanner(System.in);
-//         int n = scn.nextInt();
-//         int[] arr = new int[n];
-//         for (int i = 0; i < n; i++) {
-//             arr[i] = scn.nextInt();
-//         }
-//         int target = scn.nextInt();
-//         scn.close();
-
-//         tss(arr, 0, 0, "", target);
-
-//     }
-
-//     public static void tss(int[] arr, int idx, int ssf, String psf, int target) {
-
-//         if (ssf > target) {
-//             return;
-//         }
-
-//         if(idx==arr.length){
-//             if(ssf==target){
-//                 System.out.println(psf+".");
-//             }
-//             return;
-//         }
-//         tss(arr, idx + 1, ssf + arr[idx], psf + arr[idx] + ", ", target);
-//         tss(arr, idx + 1, ssf, psf, target);
-
-//     }
-// }
-
-import java.io.*;
 import java.util.*;
 
-public class Main {
+public class target_sum_subsets {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
@@ -48,30 +15,68 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = scn.nextInt();
         }
-        int tar = scn.nextInt();
-
+        int target = scn.nextInt();
         scn.close();
 
-        printTargetSumSubsets(arr, 0, "", 0, tar);
+        tss(arr, 0, 0, "", target);
 
     }
 
-    // set is the subset
-    // sos is sum of subset
-    // tar is target
-    public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int tar) {
+    public static void tss(int[] arr, int idx, int ssf, String psf, int target) {
 
-        if (idx >= arr.length || sos > tar) {
-            return;
-        }
-        if (sos == tar) {
-            System.out.println(set + ".");
+        if (ssf > target) {
             return;
         }
 
-        printTargetSumSubsets(arr, idx + 1, set + arr[idx] + ", ", sos + arr[idx], tar);
-        printTargetSumSubsets(arr, idx + 1, set, sos, tar);
+        if(idx==arr.length){
+            if(ssf==target){
+                System.out.println(psf+".");
+            }
+            return;
+        }
+        tss(arr, idx + 1, ssf + arr[idx], psf + arr[idx] + ", ", target);
+        tss(arr, idx + 1, ssf, psf, target);
 
     }
-
 }
+
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+
+//     public static void main(String[] args) throws Exception {
+
+//         Scanner scn = new Scanner(System.in);
+//         int n = scn.nextInt();
+//         int[] arr = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = scn.nextInt();
+//         }
+//         int tar = scn.nextInt();
+
+//         scn.close();
+
+//         printTargetSumSubsets(arr, 0, "", 0, tar);
+
+//     }
+
+//     // set is the subset
+//     // sos is sum of subset
+//     // tar is target
+//     public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int tar) {
+
+//         if (idx >= arr.length || sos > tar) {
+//             return;
+//         }
+//         if (sos == tar) {
+//             System.out.println(set + ".");
+//             return;
+//         }
+
+//         printTargetSumSubsets(arr, idx + 1, set + arr[idx] + ", ", sos + arr[idx], tar);
+//         printTargetSumSubsets(arr, idx + 1, set, sos, tar);
+
+//     }
+
+// }
