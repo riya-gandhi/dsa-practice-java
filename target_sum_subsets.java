89ee5595dc4618245b5ce800dc48d3bbaf492f08
Riya Gandhi <riya090401@gmail.com>
2022-08-25 18:47:05 +0530

    code i write vs ans for target sum subsets

--- target_sum_subsets.java
+++ target_sum_subsets.java
@@ -1,8 +1,46 @@
+// import java.util.*;
+
+// public class target_sum_subsets {
+
+//     public static void main(String[] args) {
+
+//         Scanner scn = new Scanner(System.in);
+//         int n = scn.nextInt();
+//         int[] arr = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = scn.nextInt();
+//         }
+//         int target = scn.nextInt();
+//         scn.close();
+
+//         tss(arr, 0, 0, "", target);
+
+//     }
+
+//     public static void tss(int[] arr, int idx, int ssf, String psf, int target) {
+
+//         if (ssf > target) {
+//             return;
+//         }
+
+//         if(idx==arr.length){
+//             if(ssf==target){
+//                 System.out.println(psf+".");
+//             }
+//             return;
+//         }
+//         tss(arr, idx + 1, ssf + arr[idx], psf + arr[idx] + ", ", target);
+//         tss(arr, idx + 1, ssf, psf, target);
+
+//     }
+// }
+
+import java.io.*;
 import java.util.*;
 
-public class target_sum_subsets {
+public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
@@ -10,27 +48,30 @@ public class target_sum_subsets {
         for (int i = 0; i < n; i++) {
             arr[i] = scn.nextInt();
         }
-        int target = scn.nextInt();
+        int tar = scn.nextInt();
+
         scn.close();
 
-        tss(arr, 0, 0, "", target);
+        printTargetSumSubsets(arr, 0, "", 0, tar);
 
     }
 
-    public static void tss(int[] arr, int idx, int ssf, String psf, int target) {
+    // set is the subset
+    // sos is sum of subset
+    // tar is target
+    public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int tar) {
 
-        if (ssf > target) {
+        if (idx >= arr.length || sos > tar) {
+            return;
+        }
+        if (sos == tar) {
+            System.out.println(set + ".");
             return;
         }
 
-        if(idx==arr.length){
-            if(ssf==target){
-                System.out.println(psf+".");
-            }
-            return;
-        }
-        tss(arr, idx + 1, ssf + arr[idx], psf + arr[idx] + ", ", target);
-        tss(arr, idx + 1, ssf, psf, target);
+        printTargetSumSubsets(arr, idx + 1, set + arr[idx] + ", ", sos + arr[idx], tar);
+        printTargetSumSubsets(arr, idx + 1, set, sos, tar);
 
     }
+
 }
