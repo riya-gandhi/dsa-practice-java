4c86f053bad89ec9df6e56d6a5928116414292cf
Riya Gandhi <riya090401@gmail.com>
2022-09-01 09:54:43 +0530

    climb stairs variable jump DP

---
+++ dp_cswvj_mem_practice.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class dp_cswvj_mem_practice {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        int ans = cs(n,0,arr);
+        System.out.println(ans);
+    }
+    
+    static int cs(int n,int idx,int[] arr){
+        
+        if(idx==n){
+            return 1;
+        }
+        
+        int paths = 0;
+        
+        for(int i=1;i<=arr[idx];i++){
+            if(idx+i<=n){
+                paths += cs(n,idx+i,arr);
+            }
+            
+            else break;
+        }
+        
+        return paths;
+    }
+
+}
