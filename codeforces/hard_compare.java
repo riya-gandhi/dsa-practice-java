1287065f09a9297214a8ebb06ef43b5d8e8ad5d5
Riya Gandhi <63222339+riya-gandhi@users.noreply.github.com>
2023-11-06 11:54:42 +0530

    Hard Compare cf

---
+++ codeforces/hard_compare.java
@@ -0,0 +1,35 @@
+package codeforces;
+
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+ 
+public class hard_compare {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+ 
+        long a = scn.nextLong();
+        long aPower = scn.nextLong();
+        long b = scn.nextLong();
+        long bPower = scn.nextLong();
+        scn.close();
+ 
+        if (aPower == bPower) {
+            if (a > b) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        } else {
+            double a1 = aPower * Math.log(a);
+            double b1 = bPower * Math.log(b);
+ 
+            if (a1 > b1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+ 
+    }
+}
--- leetcode/arrays/rotate_array.java
+++
@@ -1,8 +0,0 @@
-// a and b int
-// a>=b and b>=1
-
-// a/b without using 1
-
-// 4 and 2 -> 2
-// 5 and 2 - > 2
-
