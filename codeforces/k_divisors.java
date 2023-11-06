cc5039692e5213d52b3a6cbfad62273fc19caf6b
Riya Gandhi <63222339+riya-gandhi@users.noreply.github.com>
2023-11-06 12:09:45 +0530

    Fix: minor fix

--- codeforces/k_divisors.java
+++ codeforces/k_divisors.java
@@ -7,15 +7,15 @@ import java.util.Scanner;
 // then optimization
 
 public class k_divisors {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         scn.close();
 
         ArrayList<Integer> list = new ArrayList<>();
 
-        for(int i=1;i<=n;i++){
-            if(n%i == 0)    {
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
                 list.add(i);
             }
         }
