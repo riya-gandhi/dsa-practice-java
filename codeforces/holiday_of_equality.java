f60af1ca4217f588a49b39c918247b4fe9e2b6bd
Riya Gandhi <63222339+riya-gandhi@users.noreply.github.com>
2023-11-08 12:45:06 +0530

    Add: Holiday of Equality 758A

---
+++ codeforces/holiday_of_equality.java
@@ -0,0 +1,31 @@
+package codeforces;
+
+import java.util.*;
+import java.lang.Math;
+
+public class holiday_of_equality {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        scn.close();
+
+        int ans = sol(arr);
+        System.out.println(ans);
+    }
+
+    public static int sol(int[] arr) {
+
+        int max = 0, ans = 0;
+        Arrays.sort(arr);
+        max = arr[arr.length - 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            ans += max - arr[i];
+        }
+        return ans;
+    }
+}
